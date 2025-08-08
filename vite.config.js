c0eea416fb0213b121b6cf218a97974c9c9b538b
“VishnuSBhatDS” <vishnu.bhat@dealshare.in>
2025-08-08 12:23:54 +0530

    added pwa configs

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Broadcast Generator",
+        short_name: "BroadcastGen",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#00f2fe",
+        description: "Generate formatted broadcasts easily.",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
   base: './broadcast-app/',
-  build: {
-    rollupOptions: {
-      external: ['fsevents', 'fs', 'node:fs/promises'],
-    },
-    minify: false,
-  },
 });
